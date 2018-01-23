72ab122c9a531d6aa5b7d35c9be469ea2237ddb1
Charlotte Payne <cltpayne@gmail.com>
2018-01-23 01:10:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,6 +9,6 @@ function updatePlaylist(playlist, artist, song) {
 }
 
 function removeFromPlaylist(playlist, artist) {
-  delete playlist.Justin Timberlake
+  delete playlist[artist]
   return playlist;
 }
