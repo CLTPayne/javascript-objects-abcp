2d2d9ba3b9394e88a0c9ec029072f5cbb0405c06
Charlotte Payne <cltpayne@gmail.com>
2018-01-23 01:07:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,6 @@ var playlist = {
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
-  return playlist
 }
 
 function removeFromPlaylist(playlist, artistName) {
